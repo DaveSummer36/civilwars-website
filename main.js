09037d40b7c95704880637b9970d19d130e2c6c5
Dave Summer <nyaridavid345@gmail.com>
2024-11-16 21:32:44 +0100

    Bug fix #5

--- main.js
+++ main.js
@@ -6,6 +6,16 @@ let dateElement = document.getElementById('currentDate');
 let countdownElement = document.getElementById('releaseDateCountDown');
 
 function updateTimeandDate() {
+  const now = new Date();
+  
+  if(dateElement) {
+    dateElement.textContent = formatDate(now);
+  } else {
+    console.error('dateElement isn\'t defined.');
+  }
+}
+
+function updateCountdown() {
   const releaseDate = new Date('2026-08-26T12:00:00');
   const now = new Date();
   const timeDifference = releaseDate - now;
@@ -15,13 +25,7 @@ function updateTimeandDate() {
     clearInterval(countdownInterval);
     return;
   }
-
-  if(dateElement) {
-    dateElement.textContent = formatDate(now);
-  } else {
-    console.error('dateElement isn\'t defined.');
-  }
-
+    
   const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
   const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
